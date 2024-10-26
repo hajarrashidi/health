afeff0856e5225a7f7c86b5527283d71e1e4e998
Hajar <hajarrashidi@gmail.com>
2024-10-26 04:38:37 +0200

    added Hello and Oura Controller

--- src/main/java/com/veycloud/health/controller/OuraController.java
+++ src/main/java/com/veycloud/health/controller/OuraController.java
@@ -29,9 +29,6 @@ public class OuraController {
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        // Make the API request
-
-        // Return the API response
         return restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
     }
 }
